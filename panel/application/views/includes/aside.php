db52b43d932a48f6a57372353f556c58c789fe32
Mehmet Temizce <mehmettemizce@hotmail.com.tr>
2018-11-28 14:52:40 +0300

    .
    ..

--- panel/application/views/includes/aside.php
+++ panel/application/views/includes/aside.php
@@ -52,17 +52,19 @@
   <div class="menubar-scroll">
     <div class="menubar-scroll-inner">
       <ul class="app-menu">
-        <li class="has-submenu">
-          <a href="javascript:void(0)" class="submenu-toggle">
+        <li>
+          <a href="<?php echo base_url(); ?>">
             <i class="menu-icon zmdi zmdi-view-dashboard zmdi-hc-lg"></i>
             <span class="menu-text">Panel Anasayfa</span>
-            <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
+
           </a>
-          <ul class="submenu">
-            <li><a href="index.html"><span class="menu-text">Dashboard 1</span></a></li>
-            <li><a href="dashboard.2.html"><span class="menu-text">Dashboard 2</span></a></li>
-            <li><a href="dashboard.3.html"><span class="menu-text">Dashboard 3</span></a></li>
-          </ul>
+        </li>
+
+        <li>
+          <a href="#">
+            <i class="menu-icon zmdi zmdi-settings zmdi-hc-lg"></i>
+            <span class="menu-text">Panel Ayarları</span>
+          </a>
         </li>
 
         <li class="has-submenu">
@@ -228,12 +230,7 @@
           </a>
         </li>
 
-        <li>
-          <a href="javascript:void(0)">
-            <i class="menu-icon zmdi zmdi-settings zmdi-hc-lg"></i>
-            <span class="menu-text">Settings</span>
-          </a>
-        </li>
+
 
         <li>
           <a href="javascript:void(0)">
